bb613ac342ffa9ba44a581f41f29cd1d0549c59b
Admin <Admin@DESKTOP-QV9DHBL>
2023-10-18 11:30:30 +0900

    23/10/18

---
+++ OpenCVApp11/OpenCVApp11/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCVApp11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Mat m1 = Mat.Ones(3, 5, MatType.CV_8UC1);
+			Mat m2 = Mat.Zeros(3, 5, MatType.CV_8UC1);
+			Mat m3 = Mat.Eye(3, 3, MatType.CV_8UC1);
+
+			Console.WriteLine("m1 : \n" + m1.Dump());
+			Console.WriteLine("m2 : \n" + m2.Dump());
+			Console.WriteLine("m3 : \n" + m3.Dump());
+
+			m1.Dispose();
+			m2.Dispose();
+			m3.Dispose();
+		}
+	}
+}

8bd1d47585a48caff13636033468eb31316ae144
Admin <Admin@DESKTOP-QV9DHBL>
2023-10-18 17:05:18 +0900

    23/10/18

---
+++ OpenCVApp15/OpenCVApp15/Program.cs
@@ -0,0 +1,39 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCVApp15
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string path = "C:\\Temp\\img\\vtest.avi";
+			
+			VideoCapture capture = new VideoCapture(path);
+			//VideoCapture capture = new VideoCapture(0);
+			Mat frame = new Mat();
+			capture.Set(VideoCaptureProperties.FrameWidth, 640);
+			capture.Set(VideoCaptureProperties.FrameHeight, 640);
+
+			while (true)
+			{
+				if (capture.PosFrames == capture.FrameCount)
+					capture.Open(path);
+
+				capture.Read(frame);
+				Cv2.ImShow("VideoFrame", frame);
+
+				if (Cv2.WaitKey(33) == 'q')
+				break;
+			}
+
+			capture.Release();
+			Cv2.DestroyAllWindows();
+
+		}
+	}
+}
---
+++ OpenCVQuiz03/OpenCVQuiz03/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenCvSharp;
+
+namespace OpenCvQuiz03
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Mat one = new Mat("1.jpg");
+			Mat two = new Mat("2.jpg");
+			Mat three = new Mat("3.jpg");
+			Mat four = new Mat("4.jpg");
+
+			Mat left = new Mat();
+			Mat right = new Mat();
+			Mat dst = new Mat();
+
+			Cv2.VConcat(new Mat[] { one, three }, left);
+			Cv2.VConcat(new Mat[] { two, four }, right);
+			Cv2.HConcat(new Mat[] { left, right }, dst);
+
+			Cv2.ImShow("dst", dst);
+			Cv2.WaitKey();
+			Cv2.DestroyAllWindows();
+
+		}
+	}
+}

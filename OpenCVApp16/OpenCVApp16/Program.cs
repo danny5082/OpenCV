a517559de70e0dc5667822823fa215a4d62286c4
Admin <Admin@DESKTOP-QV9DHBL>
2023-10-19 11:13:28 +0900

    23/10/19

---
+++ OpenCVApp16/OpenCVApp16/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenCvSharp;
+using System;
+
+
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCVApp16
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] data = new int[9];
+			for(int i  = 0; i < data.Length; i++)
+			{
+				data[i] = i + 1;
+			}
+
+			Mat m1 = new Mat(3, 3, MatType.CV_32S, data);
+			Console.WriteLine(m1.Dump());
+		}
+	}
+}
---
+++ OpenCVApp18/OpenCVApp18/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCVApp18
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int value = 0;
+
+			//OpenCV용Window
+
+			Cv2.NamedWindow("Palette");
+		}
+	}
+}
